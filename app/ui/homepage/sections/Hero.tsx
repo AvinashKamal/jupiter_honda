d63e77827e715acd148f6511fba95d3bf482d89a
Avinash Kamal <kamalavinash26@gmail.com>
2024-05-25 15:41:49 +0530

    Added new section

--- app/ui/homepage/sections/Hero.tsx
+++ app/ui/homepage/sections/Hero.tsx
@@ -7,13 +7,12 @@ const Hero: React.FC = () => {
     return (
         <div className="hero flex flex-col text-center lg:text-left lg:flex-row gap-8 justify-between items-center">
             <div className="left">
-                <h1 className="text-[16px] text-[#808080]">Ride with us.</h1>
+                <h1 className="text-[16px] text-[#0c0c0c]">Ride with us.</h1>
                 <h1 className="text-[40px] md:text-[48px] font-bold text-[#E70323] mb-2">Jupiter Honda</h1>
-                <h2 className="text-[24px] md:text-[28px] font-medium text-[#808080] mb-2">Your Honda Service and Sales Hub</h2>
-                <button className="btn btn-primary py-4 px-12 bg-[#E70323] rounded-md text-[#f6f6f6]">Schedule a test ride</button>
+                <h2 className="text-[24px] md:text-[28px] font-medium text-[#0c0c0c] mb-2">Your Honda Service and Sales Hub</h2>
+                <button className="btn btn-primary py-4 px-12 bg-[#E70323] rounded-md text-[#f6f6f6] text-[20px]">Schedule a test ride</button>
             </div>
             <div className="right">
-
                 <Image
                  src="/images/hero-image.png" 
                  className="w-[300px] md:w-[480px] lg:w-[600px]" 
--- app/ui/homepage/sections/Stock.tsx
+++ app/ui/homepage/sections/Stock.tsx
@@ -1,8 +1,31 @@
+import Image from "next/image";
+
 const Stock = () => {
     return (
         <div>
-            <h3 className="text-center text-[40px] md:text-[48px] font-bold text-[#E70323] mb-2">Our Vehicles</h3>
-            
+            <h2 className="text-center text-[40px] md:text-[48px] font-bold text-[#E70323] mb-2">Our Vehicles</h2>
+            <div className="container flex flex-col gap-4">
+                {/* Vehicle Image */}
+                <div className="flex items-center justify-center bg-[#ececec] rounded-md">
+                    <Image
+                        src={'/images/stock.png'}
+                        width={240}
+                        height={320}
+                        className="rounded-full"
+                        alt="stock"
+                        priority
+                    />
+                </div>
+                {/* Vehicle Name */}
+                <h3 className="text-left text-[24px] md:text-[24px] font-bold text-[#E70323]">Activa</h3> 
+                {/* Vehicle Colors */}
+                <div className="colors-container p-4 bg-[#ececec] rounded-md flex gap-2 items-center">
+                  <div className="color-box w-[32px] h-[32px] rounded-full" style={{backgroundColor: '#E70323'}}></div>
+                  <div className="color-box w-[32px] h-[32px] rounded-full" style={{backgroundColor: '#000000'}}></div>
+                  <div className="color-box w-[32px] h-[32px] rounded-full" style={{backgroundColor: '#FFF'}}></div>
+                </div>
+                {/* Call to Action Button*/}
+            </div>
         </div>
     )
 }
